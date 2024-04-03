d4ef942126244a1e3bf9d377113ace486abbc0bf
vker <a2807582398@163.com>
2024-04-03 23:04:23 +0800

    fix telescope err

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,21 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
-      "AstroNvim/astrocore",
+    "AstroNvim/astrocore",
   },
   opts = function(_, opts)
-      return require("astrocore").extend_tbl(opts, {
+    local actions = require "telescope.actions"
+    return require("astrocore").extend_tbl(opts, {
       defaults = {
-          mappings = {
+        mappings = {
           i = {
-              ["<C-n>"] = actions.move_selection_next,
-              ["<C-p>"] = actions.move_selection_previous,
-              ["<C-j>"] = actions.cycle_history_next,
-              ["<C-k>"] = actions.cycle_history_prev,
-          }
-          }
-      }
-      })
-  end
+            ["<C-n>"] = actions.move_selection_next,
+            ["<C-p>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.cycle_history_next,
+            ["<C-k>"] = actions.cycle_history_prev,
+          },
+        },
+      },
+    })
+  end,
 }

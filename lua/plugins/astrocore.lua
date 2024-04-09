475fbb666c9f992bec65bb617f30111d1df3042c
vker <vker@vkerdeMacBook-Pro.local>
2024-04-09 18:14:22 +0800

    set signcol to yes

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -28,7 +28,7 @@ return {
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
-        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+        signcolumn = "yes", -- sets vim.opt.signcolumn to auto
         wrap = false, -- sets vim.opt.wrap
       },
       g = { -- vim.g.<key>

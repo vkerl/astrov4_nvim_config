b205d5f35d20f2ca54c50490865698256b0d391b
vker <vker123.@163.com>
2024-04-16 13:51:46 +0800

    add some thing

--- lua/community.lua
+++ lua/community.lua
@@ -9,6 +9,7 @@ return {
   -- import/override with your plugins folder
   -- theme
   { import = "astrocommunity.colorscheme.catppuccin" },
+
   -- languge
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.lua" },
@@ -17,16 +18,25 @@ return {
   -- { import = "astrocommunity.pack.typescript-deno" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.just" },
+
   -- motion
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.motion.marks-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
+
   -- msic
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.completion.cmp-cmdline" },
+  { import = "astrocommunity.git.git-blame-nvim" },
+
+  -- editing-support
+  -- { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.editing-support.multicursors-nvim" },
-  { import = "astrocommunity.git.git-blame-nvim" },
+
+  -- indent
+  -- { import = "astrocommunity.indent.indent-rainbowline" },
+
   -- code runner
   { import = "astrocommunity.code-runner.executor-nvim" },
 }
